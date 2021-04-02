1b8058e1b269fd7c85b0f98c9ea74c0e004c84a2
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-04-02 16:10:17 -0300

    fix: added js file and select and dates initializations but they arent working

---
+++ app.js
@@ -0,0 +1,13 @@
+// MATERIALIZE INITIALIZATIONS
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, options);
+});
+
+// HTML ELEMENTS
