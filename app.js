4827dc7b21e13c35774cf4159ac1e4d41df5c4dd
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-04-05 11:34:42 -0300

    fix: fixed materialize initialization

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // MATERIALIZE INITIALIZATIONS
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('select');
-//     var instances = M.FormSelect.init(elems, options);
-//   });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.datepicker');
-//   var instances = M.Datepicker.init(elems, options);
-// });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems);
+});
 
 // HTML ELEMENTS
 
