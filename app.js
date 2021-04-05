eb4dc0e794b5474d1087a9ab6e8258af80b7d281
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-04-05 12:11:54 -0300

    feat: finished first layout of the balance section, responsive missing

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var instances = M.FormSelect.init(elems);
   });
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.datepicker');
-  var instances = M.Datepicker.init(elems, options);
+  var instances = M.Datepicker.init(elems);
 });
 
 // HTML ELEMENTS
