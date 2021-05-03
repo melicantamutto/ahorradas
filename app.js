25774c8e186c3ea2dfe996c3afb4e4df404a13c7
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2021-05-03 16:15:25 -0300

    fix: ready to merge

--- app.js
+++ app.js
@@ -151,6 +151,7 @@ const getCategoryName = () => {
   return name;
 };
 
+// Función que busca por id el chip seleccionado y retorna el índice de la categoría con el mismo id
 const getCategoryIndex = () => {
   let index = "";
   const selected = getQuery(".selected-chip");
@@ -557,19 +558,7 @@ const showCategoryMost = (most, type) => {
 const getReport = (type) => {
   const operationsStorage = getStorage("operationsList");
   const categoriesStorage = getStorage("categoriesList");
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
+  const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov", "Dec"
   ];
   const toFilter = type === "months" ? months : categoriesStorage;
   let reportArray = [];
